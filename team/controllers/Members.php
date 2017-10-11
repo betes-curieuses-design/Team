f983e3457a28749d04e00e9a098d4d927aa08ef8
lorajc <lorajc@hotmail.com>
2017-10-11 19:09:46 -0400

    Plugin Update! Production uses

--- team/controllers/Members.php
+++ team/controllers/Members.php
@@ -15,6 +15,7 @@ class Members extends Controller
 
     public $formConfig = 'config_form.yaml';
     public $listConfig = 'config_list.yaml';
+    public $bodyClass = 'compact-container';
 
     public function __construct()
     {
